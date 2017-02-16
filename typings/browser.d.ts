1ea92aedd1d4884e4623a996e56da39e19982f28
Sebastian Telschow <s.telschow@enterfra.me>
2017-02-16 22:36:17 +0100

    quickfixed gitignores

---
+++ typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/require/index.d.ts" />
+/// <reference path="browser/definitions/moment/index.d.ts" />
+/// <reference path="browser/definitions/webpack/index.d.ts" />
