2b0b207992c887b4f39f27c354fc3721384d5f4c
Sebastian Telschow <s.telschow@enterfra.me>
2017-02-16 22:49:30 +0100

    quickfixed ignores ;-)

---
+++ src/contextMenu.item.directive.d.ts
@@ -0,0 +1,18 @@
+import { EventEmitter, TemplateRef } from '@angular/core';
+export declare class ContextMenuItemDirective {
+    template: TemplateRef<{
+        item: any;
+    }>;
+    divider: boolean;
+    enabled: boolean | ((item: any) => boolean);
+    visible: boolean | ((item: any) => boolean);
+    execute: EventEmitter<{
+        event: Event;
+        item: any;
+    }>;
+    constructor(template: TemplateRef<{
+        item: any;
+    }>);
+    evaluateIfFunction(value: any, item: any): any;
+    triggerExecute(item: any, $event?: MouseEvent): void;
+}
